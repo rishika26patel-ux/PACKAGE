fbf8e8fa77941e80ed609becb7fedaba9ebbe4af
rishika patel <https://github.com/rishika26patel-ux/GAME>
2025-11-25 15:24:36 +0530

    list

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
-import P1.A;
-import P1.B;
+//import P1.A;
+//import P1.B;
 
 
-public class Main{
+//public class Main{
+  //  public static void main(String[]args){
+    //    A s1=new A();
+      //  s1.show();
+
+        //B s2=new B();
+        //s2.show();
+    //}
+//}
+
+import java.util.ArrayList;
+class main{
     public static void main(String[]args){
-        A s1=new A();
-        s1.show();
-
-        B s2=new B();
-        s2.show();
+        ArrayList<Integer> list=new ArrayList();
+        ArrayList<Integer> list1=new ArrayList();
+        list.add(5);
+        list.add(10);
+        list.add(15);
+        list.add(1,30);
+        list.add(2,20);
+        list1.add(1);
+        list1.add(3);
+        list1.add(1,2);
+        System.out.println(list);
+        System.out.println(list1);
     }
 }
