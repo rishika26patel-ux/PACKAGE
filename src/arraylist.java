fa3cb0d723caebfdc07c0013fef642a919afa31e
rishika patel <https://github.com/rishika26patel-ux/GAME>
2025-11-27 12:59:24 +0530

    list

---
+++ src/arraylist.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+class main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList();
+        ArrayList<Integer> list1 = new ArrayList();
+        list.add(5);
+        list.add(10);
+        list.add(15);
+        list.add(1, 30);
+        list.add(2, 20);
+        list1.add(1);
+        list1.add(3);
+        list1.add(1, 2);
+        System.out.println(list);
+    }
+}
+//Access Elements
+// int first =list.get(1);
+//int last=list.get(list.size()-1);
+//list.set(2,99);//Update value
+//list.remove(0);//remove index value
+//list.remove(Integer.valueOf(15));//remove select number
+//boolean exist =list.contains(30); //Search operation
+//int idx1 =list.indexOf(20);
+//int idx2 =list.lastIndexOf(15);
+//SIZE&EMPTY
+// int size =list.size();
+//boolean isEmpyty=list.isEmpty();
+//for(int i=0;i<list.size();i++);
+//System.out.println(list.get(2));
+//ENHANCE FOR LOOP
+//for(int num:list){
+//  System.out.println(num);
+//}
+//System.out.println();
+// System.out.println(list);
+//}
+
+
---
+++ src/twosum.java
@@ -0,0 +1,23 @@
+//import java.util.*;
+//public class twosum {
+  //  public static void main(String[]args){
+
+    //    int arr[]={2,7,5,4};
+      //  HashMap<Integer,Integer>map=new HashMap();
+        //int target=12;
+        //for(int i=0;i<arr.length;i++){
+          //  int diff=target-arr[i];
+            //if(map.containsKey(diff)){
+              //  System.out.println(map.get(diff)+""+i);
+                //break;
+            //}
+            //map.put(arr[i],i);
+        //}
+    //}
+//}
+
+
+
+
+
+
