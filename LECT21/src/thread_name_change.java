0e5754068b5f830d4c86214b0e523adde2a8b3e3
rishika patel <rishika26patel>
2025-12-11 11:11:32 +0530

    pause

--- LECT21/src/thread_name_change.java
+++ LECT21/src/thread_name_change.java
@@ -1,4 +1,4 @@
-class MyThread extends Thread{
+class threadnamechange extends Thread{
     public void run(){
         System.out.println("rishiii!!");
         System.out.println(Thread.currentThread().getName());
@@ -7,14 +7,15 @@ class MyThread extends Thread{
         }
     }
 }
-public class multiple_thread {
+public class thread_name_change {
     public static void main(String[]args) {
         System.out.println(Thread.currentThread().getName());
         MyThread t1 = new MyThread();
-        //t1.setName("anshuu!");//Namechange
+        t1.setName("anshuu!");//Namechange
         t1.start();
         for(int i=0; i<5;i++){
             System.out.println(i+"tannnu!!");
         }
     }
 }
+
