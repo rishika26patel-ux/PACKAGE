92aa6e10fd7901d010076dc1ea7f08c72cb1efe5
rishika patel <rishika26patel>
2025-12-12 14:12:45 +0530

    run

---
+++ LECT22/src/limit_api.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class limit_api {
+    public static void main(String[]args){
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(50);
+        list.add(70);
+        list.add(60);
+        list.add(80);
+        list.add(90);
+        list.add(100);
+        List<Integer> limit= list.stream().limit(2).toList();
+        System.out.println(limit);
+    }
+}
+
