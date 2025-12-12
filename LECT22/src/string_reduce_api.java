4c9b75cb0254c04a38a2b1688f76db9f8e638e42
rishika patel <rishika26patel>
2025-12-12 13:36:14 +0530

    run

---
+++ LECT22/src/string_reduce_api.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class string_reduce_api {
+    public static void main(String[]args){
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(50);
+        list.add(60);
+        list.add(70);
+        list.add(80);
+        list.add(90);
+        list.add(100);
+        int data= list.stream().reduce(0,(a,b)->a+b);
+        System.out.println(data);
+    }
+}
