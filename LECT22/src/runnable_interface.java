886537223695edd1e2cac5b67d6cd26b1f733026
rishika patel <rishika26patel>
2025-12-12 13:09:16 +0530

    run

--- LECT22/src/runnable_interface.java
+++ LECT22/src/runnable_interface.java
@@ -1,22 +1,23 @@
-class thread implements Runnable{
+class thread1 implements Runnable{
     public void run(){
         for(int i=0;i<5;i++){
-           System.out.println(i);
-           try{
-               Thread.sleep(3000);
-           }catch (Exception e){
+            System.out.println(i);
+            try{
+                Thread.sleep(3000);
+            }catch (Exception e){
 
-           }
+            }
         }
         //System.out.println("rishika");
     }
 }
-public class runnable_class {
+public class runnable_interface {
     public static void main(String[]args){
-        thread task= new thread();
+        thread1 task= new thread1();
         Thread t1= new Thread(task);
         Thread t2=new Thread(task);
         t1.start();
         t2.start();
     }
 }
+
