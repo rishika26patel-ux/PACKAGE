865a01e988ce852856a94ff3ffdc14275d5b1c2f
rishika patel <rishika26patel>
2025-12-12 13:24:11 +0530

    run

--- LECT22/src/runnable_class.java
+++ LECT22/src/runnable_class.java
@@ -1,14 +1,6 @@
 class thread implements Runnable{
     public void run(){
-        for(int i=0;i<5;i++){
-           System.out.println(i);
-           try{
-               Thread.sleep(3000);
-           }catch (Exception e){
-
-           }
-        }
-        //System.out.println("rishika");
+        System.out.println("rishika");
     }
 }
 public class runnable_class {
