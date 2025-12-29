bb4bb862b74c79a6170b97b745128b3b2a01387b
rishika26patel <rishika26patel>
2025-12-29 21:36:35 +0530

    add

---
+++ twenty9_dec25/src/add_two_numbers.java
@@ -0,0 +1,11 @@
+public class add_two_numbers {
+    public static void main(String[]args){
+        int numbers1= 10;
+        int numbers2= 20;
+
+        int sum= numbers1+numbers2;
+
+        System.out.println("sum is: "+sum);
+
+    }
+}
